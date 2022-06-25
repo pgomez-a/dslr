0674dbed6d39fcde19617ee4453d6f65d441417c
pgomez-a <pablogomezalvarez02@gmail.com>
2022-06-25 13:43:57 +0200

    Update logreg_train - Regularization method removed

--- logistic_regression/logreg_train.py
+++ logistic_regression/logreg_train.py
@@ -56,7 +56,7 @@ def predict(X, theta):
     """
     return sigmoid(np.matmul(X, theta))
 
-def loss(Y, Y_hat, theta, lambda_, label):
+def loss(Y, Y_hat, theta, label):
     """
     Computes the loss for the given parameters.
     """
@@ -64,32 +64,20 @@ def loss(Y, Y_hat, theta, lambda_, label):
     Y[Y == label] = 1.
     Y_hat[Y_hat == 0.] += 1e-15
     Y_hat[Y_hat == 1.] -= 1e-15
-    l2_theta = np.copy(theta)
-    l2_theta[0][0] = 0.
-    non_reg_cost = -sum(Y * np.log(Y_hat) + (1 - Y) * np.log(1 - Y_hat)) / Y.size
-    return non_reg_cost + (lambda_ * np.matmul(l2_theta.transpose(), l2_theta)) / (2 * Y.size)
+    return -sum(Y * np.log(Y_hat) + (1 - Y) * np.log(1 - Y_hat)) / Y.size
 
-def gradient(Y, X, theta, lambda_):
-    """
-    Computes a gradient descent for the given values.
-    """
-    Y_hat = predict(X, theta)
-    cost = (Y_hat - Y).astype(float)
-    l2_theta = np.copy(theta)
-    l2_theta[0][0] = 0.
-    non_reg_grad = np.matmul(X.transpose(), cost)
-    return (non_reg_grad + (lambda_ * l2_theta)) / Y.size
-
-def train(Y, X, theta, lambda_, label):
+def train(Y, X, theta, label):
     """
     Trains the model adjusted to the desired label for the given parameters.
     """
-    alpha = 0.0001
-    max_iter = 50000
+    alpha = 0.01
+    max_iter = 10000
     Y[Y != label] = 0.
     Y[Y == label] = 1.
     for i in progress_bar(max_iter, label):
-        tmp_theta = gradient(Y, X, theta, lambda_)
+        Y_hat = predict(X, theta)
+        cost = (Y_hat - Y).astype(float)
+        tmp_theta = np.matmul(X.transpose(), cost) / Y.size
         theta -= (alpha * tmp_theta)
     return theta
 
@@ -104,12 +92,12 @@ def store_in_file(file, theta):
 if __name__ == '__main__':
     warnings.filterwarnings('ignore')
     dataset = read_dataset()
-    save_theta = open('.theta', 'w')
+    save_theta = open('weights', 'w')
     Y, X, theta = get_logreg_values(dataset)
-    raven_theta = train(np.copy(Y), X, np.copy(theta), 0.1, 'Ravenclaw') 
-    slyth_theta = train(np.copy(Y), X, np.copy(theta), 0.1, 'Slytherin')
-    gryff_theta = train(np.copy(Y), X, np.copy(theta), 0.1, 'Gryffindor')
-    huffl_theta = train(np.copy(Y), X, np.copy(theta), 0.1, 'Hufflepuff')
+    raven_theta = train(np.copy(Y), X, np.copy(theta), 'Ravenclaw') 
+    slyth_theta = train(np.copy(Y), X, np.copy(theta), 'Slytherin')
+    gryff_theta = train(np.copy(Y), X, np.copy(theta), 'Gryffindor')
+    huffl_theta = train(np.copy(Y), X, np.copy(theta), 'Hufflepuff')
     store_in_file(save_theta, raven_theta)
     store_in_file(save_theta, slyth_theta)
     store_in_file(save_theta, gryff_theta)
